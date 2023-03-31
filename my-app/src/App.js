aa9be89f2ad3ce246524c3e83c5a864c3ca07417
Robyn Smith <robyn.roybin@gmail.com>
2023-03-31 16:13:05 +0000

    Stateful class component as stateless

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps'
+import StatefulGreeting from './components/StatefulGreeting'
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Sarah" age="30" />
+      < FunctionalGreeting />
+      < FunctionalGreetingWithProps greeting="nice to meet you!" name="Sarah" age="30" />
+      < StatefulGreeting greeting="I'm a stateful class component" />
     </div>
   );
 }
