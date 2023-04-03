582675116efb258e65019bae86a653e062345fea
Robyn Smith <robyn.roybin@gmail.com>
2023-04-03 10:59:25 +0000

    Event binding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps'
-import StatefulGreeting from './components/StatefulGreeting'
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback'
+// import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import EventsClass from './components/EventsClass';
+// import EventsFunctional from './components/EventsFunctional';
+import EventBinding from './components/EventBinding';
 
 function App() {
   return (
     <div className="App">
-      < FunctionalGreeting />
+      {/* < FunctionalGreeting />
       < FunctionalGreetingWithProps greeting="nice to meet you!" name="Sarah" age="30" />
       < StatefulGreeting greeting="I'm a stateful class component" />
       < StatefulGreetingWithCallback greeting="I'm a stateful class component with callback" />
+      < EventsClass />
+      < EventsFunctional /> */}
+      < EventBinding />
     </div>
   );
 }
